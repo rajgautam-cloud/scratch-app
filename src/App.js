25d98edf521533c7fd4b6da2444fb18f449f343b
rajgautam-material <105096247+rajgautam-material@users.noreply.github.com>
2024-06-16 16:39:35 +0530

    Completed the Scratch app project

--- src/App.js
+++ src/App.js
@@ -2,18 +2,89 @@ import React from "react";
 import Sidebar from "./components/Sidebar";
 import MidArea from "./components/MidArea";
 import PreviewArea from "./components/PreviewArea";
+import { DragDropContext } from "react-beautiful-dnd";
+import { connect } from "react-redux";
+import { makeStyles } from "@material-ui/core/styles";
 
-export default function App() {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+function App({ complist, update_list }) {
+  const classes = useStyles();
+
+  // Update Lists of Mid Area
+  const onDragEnd = (result) => {
+    let element = result.draggableId.split("-")[0];
+
+    const old_list = complist.midAreaLists;
+    let source_index = old_list.findIndex(
+      (x) => x.id === result.source.droppableId
+    );
+    if (source_index > -1) {
+      let comp_list = old_list[source_index].comps;
+      comp_list.splice(result.source.index, 1);
+      old_list[source_index].comps = comp_list;
+    }
+
+    let dest_index = old_list.findIndex(
+      (x) => x.id === result.destination.droppableId
+    );
+
+    if (dest_index > -1) {
+      let dest_comp_list = old_list[dest_index].comps;
+      dest_comp_list.splice(result.destination.index, 0, `${element}`);
+
+      old_list[dest_index].comps = dest_comp_list;
+    }
+  };
   return (
-    <div className="bg-blue-100 pt-6 font-sans">
-      <div className="h-screen overflow-hidden flex flex-row  ">
-        <div className="flex-1 h-screen overflow-hidden flex flex-row bg-white border-t border-r border-gray-200 rounded-tr-xl mr-2">
-          <Sidebar /> <MidArea />
-        </div>
-        <div className="w-1/3 h-screen overflow-hidden flex flex-row bg-white border-t border-l border-gray-200 rounded-tl-xl ml-2">
-          <PreviewArea />
-        </div>
+    <div className="bg-sky-500 font-sans">
+      <header>
+        <nav class="bg-white fixed top-0 w-full border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
+          <div class="flex flex-wrap justify-between items-center max-w-screen-xl">
+            <a href="https://flowbite.com" class="flex items-center">
+              <img
+                src="https://flowbite.com/docs/images/logo.svg"
+                class="mr-3 h-6 sm:h-9"
+                alt="Scratch Logo"
+              />
+              <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+                Scratch App
+              </span>
+            </a>
+          </div>
+        </nav>
+      </header>
+      <div className="h-screen overflow-hidden flex flex-row py-10">
+        <DragDropContext onDragEnd={onDragEnd}>
+          <div className="flex-1 h-screen overflow-hidden flex flex-row bg-white border-t border-r border-gray-200 rounded-tr-xl mr-2">
+            <Sidebar />
+
+            <MidArea />
+          </div>
+          <div className="w-1/3 relative h-screen overflow-scroll flex flex-row bg-white border-t border-l border-gray-200 rounded-tl-xl ml-2">
+            <PreviewArea />
+          </div>
+        </DragDropContext>
       </div>
     </div>
   );
 }
+
+// mapping state to props
+const mapStateToProps = (state) => {
+  return {
+    complist: state.list,
+  };
+};
+
+export default connect(mapStateToProps)(App);
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,32 +1,116 @@
 import React from "react";
-import Icon from "./Icon";
+import { Draggable, Droppable } from "react-beautiful-dnd";
+import { getComponent } from "./getComponents";
+import {
+  motionComponents,
+  looksComponents,
+  controlComponents,
+} from "./SidebarConstants";
 
 export default function Sidebar() {
   return (
     <div className="w-60 flex-none h-full overflow-y-auto flex flex-col items-start p-2 border-r border-gray-200">
-      <div className="font-bold"> {"Events"} </div>
-      <div className="flex flex-row flex-wrap bg-yellow-500 text-white px-2 py-1 my-2 text-sm cursor-pointer">
-        {"When "}
-        <Icon name="flag" size={15} className="text-green-600 mx-2" />
-        {"clicked"}
-      </div>
-      <div className="flex flex-row flex-wrap bg-yellow-500 text-white px-2 py-1 my-2 text-sm cursor-pointer">
-        {"When this sprite clicked"}
-      </div>
+      {/* Motion */}
       <div className="font-bold"> {"Motion"} </div>
-      <div className="flex flex-row flex-wrap bg-blue-500 text-white px-2 py-1 my-2 text-sm cursor-pointer">
-        {"Move 10 steps"}
-      </div>
-      <div className="flex flex-row flex-wrap bg-blue-500 text-white px-2 py-1 my-2 text-sm cursor-pointer">
-        {"Turn "}
-        <Icon name="undo" size={15} className="text-white mx-2" />
-        {"15 degrees"}
-      </div>
-      <div className="flex flex-row flex-wrap bg-blue-500 text-white px-2 py-1 my-2 text-sm cursor-pointer">
-        {"Turn "}
-        <Icon name="redo" size={15} className="text-white mx-2" />
-        {"15 degrees"}
-      </div>
+      <Droppable droppableId="sideArea-motion" type="COMPONENTS">
+        {(provided) => (
+          <ul
+            className="sideArea-motion my-3"
+            {...provided.droppableProps}
+            ref={provided.innerRef}
+          >
+            {motionComponents.map((x, i) => {
+              return (
+                <Draggable
+                  key={`${x}-sideArea`}
+                  draggableId={`${x}-sideArea`}
+                  index={i}
+                >
+                  {(provided) => (
+                    <li
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                      className="my-2"
+                    >
+                      {getComponent(x)}
+                    </li>
+                  )}
+                </Draggable>
+              );
+            })}
+            {provided.placeholder}
+          </ul>
+        )}
+      </Droppable>
+
+      {/* Looks */}
+      <div className="font-bold"> {"Looks"} </div>
+      <Droppable droppableId="sideArea-looks" type="COMPONENTS">
+        {(provided) => (
+          <ul
+            className="sideArea-looks my-3"
+            {...provided.droppableProps}
+            ref={provided.innerRef}
+          >
+            {looksComponents.map((x, i) => {
+              return (
+                <Draggable
+                  key={`${x}-sideArea`}
+                  draggableId={`${x}-sideArea`}
+                  index={i}
+                >
+                  {(provided) => (
+                    <li
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                      className="my-2"
+                    >
+                      {getComponent(x)}
+                    </li>
+                  )}
+                </Draggable>
+              );
+            })}
+            {provided.placeholder}
+          </ul>
+        )}
+      </Droppable>
+
+      {/* Control */}
+      <div className="font-bold"> {"Control"} </div>
+      <Droppable droppableId="sideArea-control" type="COMPONENTS">
+        {(provided) => (
+          <ul
+            className="sideArea-control my-3"
+            {...provided.droppableProps}
+            ref={provided.innerRef}
+          >
+            {controlComponents.map((x, i) => {
+              return (
+                <Draggable
+                  key={`${x}-sideArea`}
+                  draggableId={`${x}-sideArea`}
+                  index={i}
+                >
+                  {(provided) => (
+                    <li
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                      className="my-2"
+                    >
+                      {getComponent(x)}
+                    </li>
+                  )}
+                </Draggable>
+              );
+            })}
+            {provided.placeholder}
+          </ul>
+        )}
+      </Droppable>
     </div>
   );
 }
